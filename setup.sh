09493acff52a193bf7b9dd36dbc17873241e3995
Masayoshi Michikawa <michi.omochi@gmail.com>
2013-11-19 02:28:34 +0900

    bug fix

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ DOT_FILES=(.zshrc .vimrc .bashrc .vim .tmux.conf .ssh/config)
 for file in ${DOT_FILES[@]}
 do
         # 該当の設定ファイルがすでに存在する場合は後ろに.bakをつけリネームする
-        if [! -e ${file}]; then
+        if [ ! -e ${file} ]; then
                 mv ${file} ${file}.bak
         fi
         # シンボリックリンクの作成
@@ -18,6 +18,6 @@ git submodule init
 git submodule update 
 
 # install oh-my-zsh
-if [! -d $HOME/.oh-my-zsh]
+if [ ! -d $HOME/.oh-my-zsh ]
         git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 fi
