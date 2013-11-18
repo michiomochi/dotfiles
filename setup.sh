a62044a5a1ebb144c1ccce4f20cb9339a196fc6c
Masayoshi Michikawa <michi.omochi@gmail.com>
2013-11-19 02:26:00 +0900

    setup.shを作成しました

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DOT_FILES=(.zshrc .vimrc .bashrc .vim .tmux.conf .ssh/config)
+
+for file in ${DOT_FILES[@]}
+do
+        # 該当の設定ファイルがすでに存在する場合は後ろに.bakをつけリネームする
+        if [! -e ${file}]; then
+                mv ${file} ${file}.bak
+        fi
+        # シンボリックリンクの作成
+        ln -s $HOME/dotfiles/${file} $HOME/${file}
+done
+
+# .vim submodule init & update
+cd $HOME/dotfiles/.vim
+git submodule init
+git submodule update 
+
+# install oh-my-zsh
+if [! -d $HOME/.oh-my-zsh]
+        git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+fi
