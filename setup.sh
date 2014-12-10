9607aa82d69dd776193fc68a0ee7bb80596a41dc
Michikawa Masayoshi <michikawa.masayoshi@gmail.com>
2014-12-10 22:57:51 +0900

    Update dotfiles

--- setup.sh
+++ setup.sh
@@ -1,58 +1,61 @@
 #!/bin/bash
 PWD=$(pwd)
 
-echo "Gen Simbolic Link gitconfig => ~/.gitconfig"
+echo "Gen Symbolic Link gitconfig => ~/.gitconfig"
 ln -sf ${PWD}/gitconfig ~/.gitconfig
 
-echo "Gen Simbolic Link vim => ~/.vim"
+echo "Gen Symbolic Link vim => ~/.vim"
 ln -sf ${PWD}/vim ~/.vim
 
-echo "Gen Simbolic Link vimrc => ~/.vimrc"
+echo "Gen Symbolic Link vimrc => ~/.vimrc"
 ln -sf ${PWD}/vimrc ~/.vimrc
 
-#echo "Gen Simbolic Link gvimrc => ~/.vimrc"
+#echo "Gen Symbolic Link gvimrc => ~/.vimrc"
 #ln -sf ${PWD}/gvimrc ~/.gvimrc
 
-#echo "Gen Simbolic Link vimperatorrc => ~/.vimperatorrc"
+#echo "Gen Symbolic Link vimperatorrc => ~/.vimperatorrc"
 #ln -sf ${PWD}/vimperatorrc ~/.vimperatorrc
 
-#echo "Gen Simbolic Link vimperator => ~/.vimperator"
+#echo "Gen Symbolic Link vimperator => ~/.vimperator"
 #ln -sf ${PWD}/vimperator ~/.vimperator
 
-echo "Gen Simbolic Link tmux.conf => ~/.tmux.conf"
+echo "Gen Symbolic Link tmux.conf => ~/.tmux.conf"
 ln -sf ${PWD}/tmux.conf ~/.tmux.conf
 
-echo "Gen Simbolic Link zshrc => ~/.zshrc"
+echo "Gen Symbolic Link zshrc => ~/.zshrc"
 ln -sf ${PWD}/zshrc ~/.zshrc
 
-#echo "Gen Simbolic Link zshrc.github => ~/.zshrc.github"
+#echo "Gen Symbolic Link zshrc.github => ~/.zshrc.github"
 #ln -sf ${PWD}/zshrc.github ~/.zshrc.github
 
-echo "Gen Simbolic Link zsh => ~/.zsh"
+echo "Gen Symbolic Link zsh => ~/.zsh"
 ln -sf ${PWD}/zsh ~/.zsh
 
-#echo "Gen Simbolic Link zshenv => ~/.zshenv"
+#echo "Gen Symbolic Link zshenv => ~/.zshenv"
 #ln -sf ${PWD}/zshenv ~/.zshenv
 
-#echo "Gen Simbolic Link zlogin => ~/.zlogin"
+#echo "Gen Symbolic Link zlogin => ~/.zlogin"
 #ln -sf ${PWD}/zlogin ~/.zlogin
 
-echo "Gen Simbolic Link oh-my-zsh => ~/.oh-my-zsh"
+echo "Gen Symbolic Link oh-my-zsh => ~/.oh-my-zsh"
 ln -sf ${PWD}/oh-my-zsh ~/.oh-my-zsh
 
-#echo "Gen Simbolic Link zaw => ~/.zaw"
+#echo "Gen Symbolic Link zaw => ~/.zaw"
 #ln -sf ${PWD}/zaw ~/.zaw
 
-#echo "Gen Simbolic Link irbrc => ~/.irbrc"
+#echo "Gen Symbolic Link irbrc => ~/.irbrc"
 #ln -sf ${PWD}/irbrc ~/.irbrc
 
-echo "Gen Simbolic Link gitconfig => ~/.gitconfig"
+echo "Gen Symbolic Link gitconfig => ~/.gitconfig"
 ln -sf ${PWD}/gitconfig ~/.gitconfig
 
-#echo "Gen Simbolic Link tigrc => ~/.tigrc"
+#echo "Gen Symbolic Link tigrc => ~/.tigrc"
 #ln -sf ${PWD}/tigrc ~/.tigrc
 
-#echo "Gen Simbolic Link peco_config.json => ~/.config/peco/config.json"
+#echo "Gen Symbolic Link peco_config.json => ~/.config/peco/config.json"
 #mkdir -p ~/.config/peco
 
 #ln -sf ${PWD}/peco_config.json ~/.config/peco/config.json
+
+echo "Gen Symbolic Link bin => ~/.bin"
+ln -sf ${PWD}/bin ~/.bin
